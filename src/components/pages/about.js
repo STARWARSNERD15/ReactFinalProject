378796526969ad74c452e33b15348e644df25150
STARWARSNERD15 <natey2040@gmail.com>
2021-07-14 14:44:05 -0600

    first commit

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import profilePicture from "../../../static/assets/images/bio/PSX_20200220_221211.jpg"
 
 export default function() {
     return (
-        <div>
-            About
+        <div className="content-page-wrapper">
+            <div 
+                className="left-column" 
+                style={{
+                    background: "url(" + profilePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+            />
+            <div className="right-column">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan tortor posuere ac ut consequat semper. Eget felis eget nunc lobortis mattis aliquam faucibus purus in. Urna cursus eget nunc scelerisque viverra mauris in aliquam. Arcu non sodales neque sodales ut etiam sit amet. Pellentesque id nibh tortor id aliquet lectus proin. Porttitor rhoncus dolor purus non enim praesent elementum facilisis. Adipiscing bibendum est ultricies integer quis auctor elit. Sit amet venenatis urna cursus. Turpis egestas sed tempus urna et pharetra pharetra. Quisque id diam vel quam elementum. Praesent tristique magna sit amet purus gravida. Lectus quam id leo in vitae turpis massa sed. Rutrum quisque non tellus orci.
+
+            Commodo elit at imperdiet dui accumsan sit amet nulla. Ut pharetra sit amet aliquam id diam. Quisque non tellus orci ac. Urna et pharetra pharetra massa massa ultricies mi quis hendrerit. Mollis nunc sed id semper. Id cursus metus aliquam eleifend mi in. Placerat vestibulum lectus mauris ultrices eros in cursus. Non arcu risus quis varius quam quisque. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Ultricies tristique nulla aliquet enim tortor at auctor.
+
+            Vestibulum rhoncus est pellentesque elit ullamcorper. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Mollis nunc sed id semper risus in. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Pretium viverra suspendisse potenti nullam ac tortor vitae. Urna molestie at elementum eu facilisis sed odio morbi. Praesent semper feugiat nibh sed pulvinar proin gravida. Ut porttitor leo a diam sollicitudin tempor id. Dignissim suspendisse in est ante in nibh mauris. Donec enim diam vulputate ut pharetra sit amet aliquam. Amet dictum sit amet justo donec enim diam. A diam sollicitudin tempor id eu. Odio ut enim blandit volutpat maecenas volutpat blandit.
+            </div>
         </div>
     )
 }
--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -5,61 +5,111 @@ import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
 import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            portfolioItems: []
-        };
+    this.state = {
+      portfolioItems: [],
+      portfolioToEdit: {}
+    };
 
+    this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
+    this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this);
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleEditClick = this.handleEditClick.bind(this);
+    this.clearPortfolioToEdit = this.clearPortfolioToEdit.bind(this);
+  }
 
-        this.handleSuccesfulFormSubmission = this.handleSuccesfulFormSubmission.bind(this);
-        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
-    }
+  clearPortfolioToEdit() {
+    this.setState({
+      portfolioToEdit: {}
+    })
+  }
 
-    handleSuccesfulFormSubmission(portfolioItem) {
+  handleEditClick(portfolioItem) {
+    this.setState({
+      portfolioToEdit: portfolioItem
+    })
+  }
+
+  handleDeleteClick(portfolioItem) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+        { withCredentials: true }
+      )
+      .then(response => {
         this.setState({
-            portfolioItem: [portfolioItem].concat(this.state.portfolioItems)
+          portfolioItems: this.state.portfolioItems.filter(item => {
+            return item.id !== portfolioItem.id;
+          })
         });
-    }
 
-    handleFormSubmissionError(error) {
-        console.log("handleFromSubmissionError error", error);
-    }
+        return response.data;
+      })
+      .catch(error => {
+        console.log("handleDeleteClick error", error);
+      });
+  }
 
-    getPortfolioItems() {
-        axios
-            .get("https://natetaylor.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc", {
-                withCredentials: true
-            })
-            .then(response => {
-                this.setState({
-                    portfolioItems: [...response.data.portfolio_items]
-                });
-            })
-            .catch(error => {
-            console.log("error in getPortfolioItems", error);
-            });
-    }
+  handleEditFormSubmission() {
+    this.getPortfolioItems();
+  }
+  
+  handleNewFormSubmission(portfolioItem) {
+    this.setState({
+      portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+    });
+  }
 
-    componentDidMount() {
-        this.getPortfolioItems();
-    }
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError error", error);
+  }
 
-    render() {
-        return (
-            <div className="portfolio-manager-wrapper">
-                <div className="left-column">
-                    <PortfolioForm 
-                        handleSuccesfulFormSubmission = {this.handleSuccesfulFormSubmission}
-                        handleFormSubmissionError = {this.handleFormSubmissionError}
-                    />
-                </div>
+  getPortfolioItems() {
+    axios
+      .get(
+        "https://natetaylor.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        {
+          withCredentials: true
+        }
+      )
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
 
-                <div className="right-column">
-                    <PortfolioSidebarList data={this.state.portfolioItems}/>
-                </div>    
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    return (
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <PortfolioForm
+            handleNewFormSubmission={this.handleNewFormSubmission}
+            handleEditFormSubmission={this.handleEditFormSubmission}
+            handleFormSubmissionError={this.handleFormSubmissionError}
+            clearPortfolioToEdit={this.clearPortfolioToEdit}
+            portfolioToEdit={this.state.portfolioToEdit}
+          />
+        </div>
+
+        <div className="right-column">
+          <PortfolioSidebarList
+            handleDeleteClick={this.handleDeleteClick}
+            data={this.state.portfolioItems}
+            handleEditClick={this.handleEditClick}
+          />
+        </div>
+      </div>
+    );
+  }
 }
